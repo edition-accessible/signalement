037a7f8e93ba286298e84ac65ea75f05262e3720
Gautier <38312303+editadapt@users.noreply.github.com>
2021-11-25 09:21:17 +0100

    livraison initiale

---
+++ journal.md
@@ -0,0 +1,73 @@
+## todo
+- isoler css a11y
+
+## du 15 au 18 novembre 2021
+Collecte de références et d'exemples : 
+
+https://benetechaccessiblebooks.vitalsource.com/
+
+https://catalogo.fondazionelia.org/content/michel-foucault-il-filosofo-del-secolo-una-biografia-9788858842089#accessibilityList
+
+https://nnels.ca/items/amazing-black-atlantic-canadians-inspiring-stories-courage-and-achievement
+
+https://nnels.ca/understanding-accessibility-features
+
+Certified Accessible:
+
+    Yes (30)
+
+Filter by Accessibility:
+
+    Described images (375)
+    Described math (1)
+    Customizable display (607)
+    ​Print page numbers (127)
+    Heading navigation (877)
+    Table of contents navigation (883)
+    None provided (158)
+
+https://www.w3.org/Translations/WCAG20-fr/
+
+ Criterion 1.1.1 Non-text Content (Level A): All non-text content that is presented to the user has a text alternative that serves the equivalent purpose
+
+ Note that prerecorded audio-only and prerecorded video-only files are covered here. 
+
+ https://www.w3.org/WAI/WCAG21/Techniques/general/G69
+
+ creating a document that tells the same story and presents the same information as the synchronized media
+
+ (adresse les sourds)
+
+Règle 1.2 Média temporel : proposer des versions de remplacement aux médias temporels.
+
+Comprendre la règle 1.2 (en anglais)
+
+1.2.1 Contenu seulement audio ou vidéo (pré-enregistré) : pour des médias pré-enregistrés seulement audio et pré-enregistrés seulement vidéo, sauf si l'audio ou la vidéo sont un média de remplacement pour un texte et qu'ils sont clairement identifiés comme tels : (Niveau A)
+
+    Contenu pré-enregistré seulement audio : fournir une version de remplacement pour un média temporel, présentant une information équivalente au contenu seulement audio.
+
+    Contenu pré-enregistré seulement vidéo : fournir, soit une version de remplacement pour un média temporel, soit une piste audio (présentant une information équivalente) pour un contenu pré-enregistré seulement vidéo.
+
+
+
+    Success Criterion 1.2.3: Audio Description or Media Alternative (Prerecorded) (Sufficient using a more specific technique)
+    Success Criterion 1.2.8: Media Alternative (Prerecorded) (Sufficient using a more specific technique)
+
+
+Révision du dispositif de modale accessible
+
+## 19/11/21
+## 24/11/21
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ statics/modale-def.js
@@ -0,0 +1,96 @@
+"use strict";
+
+
+/* *** Fenêtre modale *** */
+/* ***
+On créé les variables pour stocker les objets du dispositif :
+- la modale, 
+- le bouton de contrôle, 
+- le bouton de fermeture, 
+- le dernier élément de la modale pouvant recevoir le focus, 
+- le wrapper du contenu qui devra être inhibé par aria-hidden='true' tant que la modale est affichée.
+	
+Propriétés ARIA utilisées :
+- Sur la zone de contenu
+-- aria-hidden='true'
+* ***/
+var modalContent = document.getElementById('modal-def-content');
+var modalBtn = document.getElementById('modal-def');
+var modalClose = document.getElementById('btn-close-def');
+var lastFocus = lastElem(modalContent);
+var modalWrapper = document.getElementById('wrapper');
+//traitement
+/* ***
+Configuration de la modale. À l'ouverture  :
+- la modale est rendue visible,
+- la restitution du contenu est inhibée,
+- le focus est donné au premier élément pouvant le recevoir dans la modale (le bouton de fermeture),
+- l'événement focus est surveillé pour le contraindre dans la modale,
+- l'événement click est surveillé pour empêcher les actions de click sur le contenu,
+- la touche escape, qui doit fermer la modale, est surveillée,
+- l'événement de fermeture est affecté au bouton de fermeture,
+- la tabulation est surveillée pour assurer la navigation circulaire dans la modale.
+*** */
+modalBtn.addEventListener('click', function() {
+    //open/close
+    modalContent.classList.add('is-visible');
+    modalWrapper.setAttribute('aria-hidden', 'true');
+    modalClose.focus();
+    //trapping focus
+    document.addEventListener('focus', trapFocus, true);
+    //pas de click en dehors de la modale
+    document.addEventListener('click', noClick, true);
+    //fermeture via escape
+    document.addEventListener('keydown', function(event) {
+        if (event.keyCode === 27) stopListening();
+    }, false);
+    //bouton de fermeture
+    modalClose.addEventListener('click', stopListening, false);
+    //Tabulation circulaire : 
+    // si la fenêtre à le focus, en tabulation arrière on doit redonner le focus sur le dernier élément (variable 'lastFocus')
+    document.addEventListener('keyup', function(event) {
+        var curEle = document.activeElement;
+        if (curEle === modalContent) {
+            if (event.shiftKey && event.keyCode === 9) {
+                lastFocus.focus();
+            }
+            event.stopPropagation();
+        }
+    }, false);
+}, false);
+/* *** Dépendances *** */
+//Récupère le dernier élément de la fenêtre
+function lastElem(modale) {
+    var eleFocus = modale.getElementsByTagName('*'); //récupérer tous les éléments de la modale	
+    for (var i = 0, len = eleFocus.length; i < len; i++) {
+        if (eleFocus[i].tabIndex >= '0') { //On teste si l'élément peut recevoir le focus	
+            lastFocus = eleFocus[i]; // Si oui, on affecte l'élément à 'lastFocus'
+        }
+    };
+    return lastFocus;
+}
+//trapping focus
+function trapFocus(event) {
+    console.log('event focus');
+    if (!modalContent.contains(event.target)) {
+        console.log('focus !');
+        modalContent.focus();
+    }
+};
+//désactivation du click
+function noClick(event) {
+    console.log('event click');
+    if (!modalContent.contains(event.target)) {
+        console.log('no click');
+        event.stopPropagation();
+        event.preventDefault();
+    }
+};
+//fermeture, rétablissement de la restitution du contenu, destruction des listener focus et click, remise du focus sur le bouton de contrôle
+function stopListening() {
+    modalContent.classList.remove('is-visible');
+    modalWrapper.removeAttribute('aria-hidden');
+    document.removeEventListener('focus', trapFocus, true);
+    document.removeEventListener('click', noClick, true);
+    modalBtn.focus();
+};
